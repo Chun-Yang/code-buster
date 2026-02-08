4103782535b9dc6bfca2503a3bcbf1241f8088f1
Chun-Yang <yang2007chun@gmail.com>
2026-02-08 17:17:44 -0500

    Revert "move keyboard to the bottom"
    This reverts commit 906cd9b406828956f916bdd7aafde26971ad3811.

--- src/pages/MorseExercise.tsx
+++ src/pages/MorseExercise.tsx
@@ -211,7 +211,7 @@ export default function MorseExercise() {
   }
 
   return (
-    <div className="exercise-page">
+    <div>
       <div className="exercise-header">
         <button className="btn btn-back" onClick={() => setLocation('/morse-config')}>
           Back
